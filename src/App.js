838b4b45a71d4f68b364620e49e9856357e98d51
Christian Sandford <christian.sandford25@gmail.com>
2022-11-16 13:46:33 -0600

    add info from old webpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import './App.css';
-import { AppBar, Box, IconButton, Toolbar } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import ResponsiveAppBar from './components/ResponsiveAppBar';
+import "./App.css";
+import { Box } from "@mui/material";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
 
 function App() {
   return (
     <Box>
       <ResponsiveAppBar />
+      <Box>
+        <h2>
+          <u>Tech Employment History</u>
+        </h2>
+        <h3>General Motors (July 2021 - Present)</h3>
+        <h4>Role : Software Developer - Full Stack</h4>
+        <h4>Accomplishments</h4>
+        <li>Worked on Advisor Workbench Software</li>
+        <li>Unit Testing</li>
+        <li>Client Support Tickets</li>
+        <li>Completed SAFE Agile training and acquired certification</li>
+        <h4>Tech Stack</h4>
+        <li>JavaScript, ReactJS</li>
+        <li>Java, Spring Framework</li>
+        <li>Oracle Siebel</li>
+
+        <h2>
+          <u>Cosmic Cove Games</u>
+        </h2>
+        <h3>Cardboard Samurai</h3>
+        <h3>Galactic Dungeon RL</h3>
+
+        <h2>
+          <u>Novels</u>
+        </h2>
+        <h3>The Gangs of Nexon - Upcoming</h3>
+      </Box>
     </Box>
   );
 }
