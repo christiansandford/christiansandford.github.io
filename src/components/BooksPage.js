9e54ec7fc24d09d1a257d8943b3c42a7b2d2dd8c
Christian Sandford <christian.sandford25@gmail.com>
2022-11-16 18:51:11 -0600

    made site navigatable

---
+++ src/components/BooksPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+function BooksPage() {
+    return (
+        <Box>
+            Books page
+        </Box>
+    )
+}
+
+export default BooksPage;
--- src/components/ResponsiveAppBar.js
+++ src/components/ResponsiveAppBar.js
@@ -3,22 +3,17 @@ import { AppBar, Box, Button, MenuItem } from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import AdbIcon from "@mui/icons-material/Adb";
 
 const pages = ["Resume", "Books", "Games"];
 
-function ResponsiveAppBar() {
+function ResponsiveAppBar({ setActivePage }) {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
+  const handleNavButtonClick = (page) => {
     setAnchorElNav(null);
+    setActivePage(page);
   };
 
   return (
@@ -43,52 +38,19 @@ function ResponsiveAppBar() {
           >
             Christian Sandford
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
+            {pages.map((page) => {
+              console.log('Added Page', page);
+              return (
+                <Button
+                  key={page}
+                  onClick={() => handleNavButtonClick(page)}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              );
+            })}
           </Box>
         </Toolbar>
       </Container>
