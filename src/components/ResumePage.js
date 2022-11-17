93a922dabc0a59ebfa0dcd4a9682ad5da52cdac2
Christian Sandford <christian.sandford25@gmail.com>
2022-11-17 12:19:06 -0600

    changed resume to use mui components

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -1,34 +1,42 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, List, ListItemText, Typography } from "@mui/material";
+import GMLogo from "../img/general-motors-logo.png"
 
 function ResumePage() {
   return (
-    <Box backgroundColor="default" textAlign="center">
-      <h2>
-        <u>Tech Employment History</u>
-      </h2>
-      <h3>General Motors (July 2021 - Present)</h3>
-      <h4>Role : Software Developer - Full Stack</h4>
-      <h4>Accomplishments</h4>
-      <li>Worked on Advisor Workbench Software</li>
-      <li>Unit Testing</li>
-      <li>Application Support Tickets</li>
-      <li>Completed SAFE Agile training and acquired certification</li>
-      <h4>Tech Stack</h4>
-      <li>JavaScript, ReactJS</li>
-      <li>Java, Spring Framework</li>
-      <li>Oracle Siebel</li>
-
-      <h2>
-        <u>Cosmic Cove Games</u>
-      </h2>
-      <h3>Cardboard Samurai</h3>
-      <h3>Galactic Dungeon RL</h3>
-
-      <h2>
-        <u>Novels</u>
-      </h2>
-      <h3>The Gangs of Nexon - Upcoming</h3>
+    <Box display="flex" flexDirection="column" backgroundColor="default">
+        <Box display="flex" justifyContent="center">
+          <Typography variant="h1">Resume</Typography>
+        </Box>
+        <Box display="flex" justifyContent="center">
+          <Typography variant="h2">Tech Employment History</Typography>
+        </Box>
+        <Box display="flex" justifyContent="center">
+          <img src={GMLogo} alt="gm_logo" width="100" height="100" />
+          <Box marginLeft={1}>
+            <Typography variant="h3">General Motors (July 2021 - Present)</Typography>
+            <Typography variant="h4">Role: Software Developer - Full Stack</Typography>
+          </Box>
+        </Box>
+        <Box display="flex" flex-direction="column" justifyContent="center" alignItems="center" px={2}>
+          <Box>
+            <Typography variant="h4">Accomplishments</Typography>
+            <List>
+              <ListItemText>Worked on Advisor Workbench Software</ListItemText>
+              <ListItemText>Unit testing</ListItemText>
+              <ListItemText>Application support tickets</ListItemText>
+              <ListItemText>Completed SAFE Agile training and acquired certification</ListItemText>
+            </List>
+          </Box>
+          <Box>
+            <Typography variant="h4">Tech Stack</Typography>
+            <List>
+              <ListItemText>Javascript, React</ListItemText>
+              <ListItemText>Java, Spring Framework</ListItemText>
+              <ListItemText>Oracle Siebel</ListItemText>
+            </List>
+          </Box>
+        </Box>
     </Box>
   );
 }
