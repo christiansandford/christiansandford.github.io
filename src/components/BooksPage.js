afc6699b90e3eb56e882a25185321c9464a32223
Christian Sandford <christian.sandford25@gmail.com>
2023-01-12 15:10:34 -0600

    create book card

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,33 +1,41 @@
 import React from "react";
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import GoNCover from "../img/gangs_of_nexon_cover.png";
 
 const gonDescription = "In the city of Nexon only greed wins.";
 
+/**
+ * Books Page Component
+ * @returns null
+ */
 function BooksPage() {
   return (
-    <Box
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-    >
-      <Paper
-        display="flex"
-        justifyContent="center"
-        class="cover"
-        style={{
-          backgroundImage: `url(${GoNCover})`,
-          backgroundSize: "30%",
-          backgroundRepeat: "no-repeat",
-          height: "60vh",
-          color: "#f5f5f5",
-        }}
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        title="The Gangs of Nexon"
+        subheader={gonDescription}
+        sx={{ textAlign: "center" }}
       />
-      <Box display="flex" flexDirection="column" justifyContent="center">
-        <Typography variant="h3">The Gangs of Nexon</Typography>
-        <Typography variant="p1">{gonDescription}</Typography>
-      </Box>
-    </Box>
+      <CardMedia 
+        sx={{ height: 250 }}
+        image={GoNCover}
+        title="Gangs of Nexon"
+      />
+      <CardContent sx={{ textAlign: 'center' }}>
+        <Typography
+          variant="body2"
+          sx={{
+            fontWeight: 'bold',
+            textAlign: 'center',
+          }}
+        >
+          Releasing 2023
+        </Typography>
+        <Button disabled={true} variant="contained">
+          Pre-order Now
+        </Button>
+      </CardContent>
+    </Card>
   );
 }
 
