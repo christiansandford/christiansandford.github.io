4b40eaec0ebf2d0999810247c8f80b683b2598c2
Christian Sandford <christian.sandford25@gmail.com>
2023-01-12 15:21:03 -0600

    center books page

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Button, Card, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+import { Button, Card, CardContent, CardHeader, CardMedia, Grid, Typography } from "@mui/material";
 import GoNCover from "../img/gangs_of_nexon_cover.png";
 
 const gonDescription = "In the city of Nexon only greed wins.";
@@ -10,32 +10,42 @@ const gonDescription = "In the city of Nexon only greed wins.";
  */
 function BooksPage() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        title="The Gangs of Nexon"
-        subheader={gonDescription}
-        sx={{ textAlign: "center" }}
-      />
-      <CardMedia 
-        sx={{ height: 250 }}
-        image={GoNCover}
-        title="Gangs of Nexon"
-      />
-      <CardContent sx={{ textAlign: 'center' }}>
-        <Typography
-          variant="body2"
-          sx={{
-            fontWeight: 'bold',
-            textAlign: 'center',
-          }}
-        >
-          Releasing 2023
-        </Typography>
-        <Button disabled={true} variant="contained">
-          Pre-order Now
-        </Button>
-      </CardContent>
-    </Card>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Grid item>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardHeader
+            title="The Gangs of Nexon"
+            subheader={gonDescription}
+            sx={{ textAlign: "center" }}
+          />
+          <CardMedia 
+            sx={{ height: 250 }}
+            image={GoNCover}
+            title="Gangs of Nexon"
+          />
+          <CardContent sx={{ textAlign: 'center' }}>
+            <Typography
+              variant="body2"
+              sx={{
+                fontWeight: 'bold',
+                textAlign: 'center',
+              }}
+            >
+              Releasing 2023
+            </Typography>
+            <Button disabled={true} variant="contained">
+              Pre-order Now
+            </Button>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
 
--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -1,42 +1,46 @@
 import React from "react";
-import { Box, List, ListItemText, Typography } from "@mui/material";
+import { Box, Card, List, ListItemText, Typography } from "@mui/material";
 import GMLogo from "../img/general-motors-logo.png"
 
+/**
+ * Resume Page Component
+ * @returns null
+ */
 function ResumePage() {
   return (
     <Box display="flex" flexDirection="column" backgroundColor="default">
-        <Box display="flex" justifyContent="center">
-          <Typography variant="h1">Resume</Typography>
+      <Box display="flex" justifyContent="center">
+        <Typography variant="h1">Resume</Typography>
+      </Box>
+      <Box display="flex" justifyContent="center">
+        <Typography variant="h2">Tech Employment History</Typography>
+      </Box>
+      <Box display="flex" justifyContent="center">
+        <img src={GMLogo} alt="gm_logo" width="100" height="100" />
+        <Box display="flex" flexDirection="column" marginLeft={1}>
+          <Typography variant="p1">General Motors (July 2021 - Present)</Typography>
+          <Typography variant="p2">Role: Software Developer - Full Stack</Typography>
         </Box>
-        <Box display="flex" justifyContent="center">
-          <Typography variant="h2">Tech Employment History</Typography>
+      </Box>
+      <Box display="flex" flex-direction="column" justifyContent="center" alignItems="center" px={2}>
+        <Box>
+          <Typography variant="h4">Accomplishments</Typography>
+          <List>
+            <ListItemText>Worked on Advisor Workbench Software</ListItemText>
+            <ListItemText>Unit testing</ListItemText>
+            <ListItemText>Application support tickets</ListItemText>
+            <ListItemText>Completed SAFE Agile training and acquired certification</ListItemText>
+          </List>
         </Box>
-        <Box display="flex" justifyContent="center">
-          <img src={GMLogo} alt="gm_logo" width="100" height="100" />
-          <Box display="flex" flexDirection="column" marginLeft={1}>
-            <Typography variant="p1">General Motors (July 2021 - Present)</Typography>
-            <Typography variant="p2">Role: Software Developer - Full Stack</Typography>
-          </Box>
+        <Box>
+          <Typography variant="h4">Tech Stack</Typography>
+          <List>
+            <ListItemText>Javascript, React</ListItemText>
+            <ListItemText>Java, Spring Framework</ListItemText>
+            <ListItemText>Oracle Siebel</ListItemText>
+          </List>
         </Box>
-        <Box display="flex" flex-direction="column" justifyContent="center" alignItems="center" px={2}>
-          <Box>
-            <Typography variant="h4">Accomplishments</Typography>
-            <List>
-              <ListItemText>Worked on Advisor Workbench Software</ListItemText>
-              <ListItemText>Unit testing</ListItemText>
-              <ListItemText>Application support tickets</ListItemText>
-              <ListItemText>Completed SAFE Agile training and acquired certification</ListItemText>
-            </List>
-          </Box>
-          <Box>
-            <Typography variant="h4">Tech Stack</Typography>
-            <List>
-              <ListItemText>Javascript, React</ListItemText>
-              <ListItemText>Java, Spring Framework</ListItemText>
-              <ListItemText>Oracle Siebel</ListItemText>
-            </List>
-          </Box>
-        </Box>
+      </Box>
     </Box>
   );
 }
